51e139b84040cc444e085d733bbb9f55241de011
Irina Pulbere <263441962+dissNaBabana@users.noreply.github.com>
2026-05-23 13:43:52 +0300

    Initial commit

---
+++ src/test/java/md/ceiti/spring/TravelAgencyRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package md.ceiti.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAgencyRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
